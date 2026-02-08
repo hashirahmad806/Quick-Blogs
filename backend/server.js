bb1a330074607bd78c794131eacb7abb0c7df34b
Hashir Ahmad <hashirahmad806@gmail.com>
2026-02-08 17:10:08 +0500

    Update server configuration

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';  
+import ConnectionDB from './Connection/db.js';
+import adminRouter from './routes/AdminRoutes.js';
+import blogRouter from './routes/blogRoute.js';
 
 dotenv.config();
 
@@ -10,18 +13,35 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
+ 
+// Routes
 app.get('/', (req, res) => {
     res.send('Hello from the backend server!');
 } 
 );
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`); 
+app.use('/api/admin' , adminRouter)
+app.use('/api/blog' , blogRouter)
 
- });
 
+ConnectionDB().then(() => {
+
+    try {
+        console.log("DB Connected  sucessfull" );
+        app.listen(PORT, () => {
+    
+            console.log(`Server is running on http://localhost:${PORT}`);
+
+        });
+    } catch (error) {
+         console.log( "Error  While   Connection in DataBase " + "" + error  )
+        
+    }
+    
+});
+    
+ 
 
   
 
-  
+  export default app;
